b11fe6c026297cf4fb1a84f537e1c31feb010575
patxiofromalphensign <aissypewdiepie@gmail.com>
2021-06-18 20:31:04 +0530

    wokred around segFault ; poorly

--- socketio.c
+++ socketio.c
@@ -14,7 +14,7 @@
 // TODO make homogenous type struct for iterating server meta_data
 
 struct server_status {
-	uint16_t *test, *addr, *port;
+	uint16_t *test, *addr, *port, *fuck;
 	uint16_t arr[4];
 };
 
@@ -49,14 +49,14 @@ void get(int sockfd){ // iteraction
 void iter_server(union  iter_server_status *i) {
 	uint16_t *names[] = {(uint16_t*)"",(uint16_t*)"localhost", (uint16_t*)"8080"};
 	union  iter_server_status* iter = (union iter_server_status*)malloc(sizeof(union iter_server_status)); 
-	i->addr[0] = names[0]; //segFault on assignment
+	memcpy(i, names, sizeof(i));
 
-	for(int idx=0; i->arr[idx]!=NULL;i++) {
+	for(int idx=0; i->arr[idx];i++) {
+		//((uint16_t*)i->arr)[idx] = *names[idx]; //segFault on assignment
+		i->arr[idx] = *names[idx]; //segFault on assignment
 		//if(names[idx]) i->arr[idx] = &names[idx] ; //segFault
-		printf("1");
 	}
 }
-
 int main() {
 	struct sockaddr_in cli, serveraddr;
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -70,21 +70,23 @@ int main() {
 		cli.sin_port =  8081;
 	}
 
-	printf("trying to bind to %d ", inet_addr("192.168.29.167"));
+	/* printf("trying to bind to %d ", inet_addr("192.168.29.167")); */
 
-	if (!bind(sockfd, (SA*)&cli, sizeof(cli)))  {
+	if (bind(sockfd, (SA*)&cli, sizeof(cli)))  {
 		printf("failed\n");
 		exit(0);
 	}
-	if(listen(sockfd, 5)!=0) {printf("listen falied"); exit(0);}
+
+	/* if(listen(sockfd, 5)!=0) {printf("listen falied"); exit(0);} */
 
 	int len = sizeof(serveraddr);
 	int connfd =accept(sockfd, (SA*)&cli, &len);
 	// break : setup_server_status
-	union iter_server_status *isv, *isv_prime;
-	iter_server(isv);
+	union iter_server_status isv, isv_prime;
+	struct server_status ss;
+	iter_server(&isv);
+	printf("%d",(char)isv.arr[1]);
 	//get(connfd);
 	return 0;
 } 
-
 
